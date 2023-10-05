643ae483eefb08d0d888f2fc2178ebcdc77233e1
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2023-10-05 10:28:41 +0200

    f

--- react-navigation-app/src/components/about/About.js
+++ react-navigation-app/src/components/about/About.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SolarSmart from "../../images/solarsmart.png"
+import Meg from "../../images/meg.png";
+import Footer from '../nav/Footer';
 
-const About = () => <h1>Homo</h1>;
+
+function About() {
+    return (
+    <body className='bakgrunn'>
+    <div className='container'>
+    <div className='projects'>
+    <div className='project'>
+        <img className='solarsmart' src={SolarSmart} alt='image of solar smart website'/>
+        <h3> Solar smart</h3>
+        <p>Solar smart is my ownproject i creayed on a school project.</p>
+    </div>
+    </div>
+    <div className='image'>
+    <img className='meg' src={Meg} alt="sneek peak of project"/>
+    </div>
+      </div>
+    <Footer></Footer>  
+    </body>
+    
+  );
+  }
+
 
 export default About;
--- react-navigation-app/src/components/home/Home.js
+++ react-navigation-app/src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Meg from "../../images/meg.png";
 import Footer from '../nav/Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
     <div className='info'>
     <h1>Welcome to my portfolio!</h1>
     <p>A student who are finished with my frontend development studies at Noroff. My focus is on writing clean and efficient code. I am a big fan of React, HTML5, CSS/SCSS and plain Javascript. </p>
+    <Link to="/about"><a className='cta-button'>My work</a></Link>
     </div>
     <div className='image'>
     <img className='meg' src={Meg} alt="sneek peak of project"/>
